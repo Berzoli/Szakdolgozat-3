94bf4bcb2e1e5c58e58f20e1fbb47a8420a65361
csipakhh <dnorbert99@gmail.com>
2022-01-25 16:34:57 +0100

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,9 +1,15 @@
 <?php 
-
+/* root jelszo nelkul*/
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'demo');
 
+/* csatlakozas a MySql adatbazishoz*/
+$link = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
+// csatlakozas ellenorzese
+if ($link === false) {
+	die("ERROR: Nem tudsz csatlakozni." . mysqli_connect_error());
+}
  ?>
