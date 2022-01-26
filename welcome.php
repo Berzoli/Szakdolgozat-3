fd84b1948c2cbf477d31e3f9a34da71b4811de32
Zoli <berzoli@gmail.com>
2022-01-26 18:57:12 +0100

    welcome update

--- welcome.php
+++ welcome.php
@@ -1,3 +1,5 @@
+<?php include_once('header.php'); ?>
+
 <?php
 // Initialize the session
 session_start();
@@ -8,3 +10,21 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 ?>
+
+
+
+    <table class="menus">
+        <tr>
+            <td>Gyakorló mód</td>
+            <td>Vizsga mód</td>
+        </tr>
+        <tr>
+            <td>Táblák gyakorlása</td>
+            <td>Szituációs kérdések</td>
+        </tr>
+        <tr>
+            <td colspan="2">Korábbi eredmények</td>
+        </tr>
+    </table>
+
+<?php include_once('footer.php'); ?>
